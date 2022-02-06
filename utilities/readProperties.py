e1d143e1f99eb6780543f8de31aa49756c13f22a
Vikas Sandeep Kumar <vikassandeepkumar@gmail.com>
2022-02-06 23:09:43 +0530

    Demo

---
+++ utilities/readProperties.py
@@ -0,0 +1,23 @@
+import configparser
+
+config=configparser.RawConfigParser()
+config.read(".\\Configurations\\config.ini")
+
+class ReadConfig:
+    @staticmethod
+    def getApplicationURL(self):
+        url=config.get('common info', 'baseURL')
+        return url
+
+
+    @staticmethod
+    def getUseremail(self):
+        username = config.get('common info', 'useremail')
+        return username
+
+
+    @staticmethod
+    def getPassword(self):
+        password = config.get('common info', 'password')
+        return password
+
